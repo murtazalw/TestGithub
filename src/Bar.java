b59b858a15a2c5fe4e2958876492dacb19fac713
murtazalw <murtaza_lw@yahoo.com>
2016-04-25 11:00:45 +0300

    my first commit

---
+++ src/Bar.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Murtaza on 24-Apr-2016.
+ */
+public class Bar {
+    int b;
+    String s, s2, s3;
+}
---
+++ src/Foo.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Murtaza on 24-Apr-2016.
+ */
+public class Foo {
+    int f;
+}
